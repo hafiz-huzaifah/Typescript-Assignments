8a9430d751a7ff9103b314d37a1ec68ee5d5e59a
hafiz-huzaifah <164788023+hafiz-huzaifah@users.noreply.github.com>
2024-03-31 16:25:15 +0500

    Add files via upload

---
+++ 22 International_Error.ts
@@ -0,0 +1,13 @@
+
+// if you haven't recievied an array index error in one of your program yet , try to make one happen.
+
+// creating array
+let newArray :string[] = ['Baber ','Rizwan','Shadab'];
+
+//Accesing array by index
+console.log(newArray[5]);
+
+//make sure you correct the error before closing the ptogram.
+
+//correct the error.
+console.log(newArray);
