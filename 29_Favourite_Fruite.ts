971014375346ad71e455cfeedf25addf2c4abeaf
hafiz-huzaifah <164788023+hafiz-huzaifah@users.noreply.github.com>
2024-03-31 16:30:10 +0500

    Add files via upload

---
+++ 29_Favourite_Fruite.ts
@@ -0,0 +1,17 @@
+let favorite_fruit: string[] = ["mango","banana","apple"];
+
+if(favorite_fruit.includes("mango")){
+    console.log("I really like mango");
+}
+if(favorite_fruit.includes("banana")){
+    console.log("I really like banana");
+}
+if(favorite_fruit.includes("apple")){
+    console.log("I really like apple");
+}
+if(favorite_fruit.includes("orange")){
+    console.log("I really like orange");
+}
+if(favorite_fruit.includes("dates")){
+    console.log("I really like dates");
+}
