2ad70b58a30a8818a8f3e4cf884effbaeb9a436e
hafiz-huzaifah <164788023+hafiz-huzaifah@users.noreply.github.com>
2024-03-31 16:12:24 +0500

    Add files via upload

---
+++ 13 Your_Own_Array.ts
@@ -0,0 +1,9 @@
+
+
+let transportation :string[] = ['Motercycle','Car','Train','Cycle'] ;
+ 
+for(let i=0; i<transportation.length; i++){
+
+    console.log('I would like to own a '+ transportation[i])
+    
+}
