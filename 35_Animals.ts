221031792721558f85cd9a709a39f2275ef8804c
hafiz-huzaifah <164788023+hafiz-huzaifah@users.noreply.github.com>
2024-03-31 16:34:26 +0500

    Add files via upload

---
+++ 35_Animals.ts
@@ -0,0 +1,10 @@
+
+
+let Animals: string[] = ["dog","cat","rabbit"];
+
+for(let Animal of Animals){
+
+   console.log(`A ${Animal} would make a great pet.`);
+}
+
+console.log(`Any of these animals would a great pet!`);
