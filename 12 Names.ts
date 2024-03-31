81bf139492bb4f25cf61208b30c64fd5c236c539
hafiz-huzaifah <164788023+hafiz-huzaifah@users.noreply.github.com>
2024-03-31 16:11:34 +0500

    Add files via upload

---
+++ 12 Names.ts
@@ -0,0 +1,11 @@
+
+
+let Names :string[] = ['Taha','Husnain','Hasan','Hanzalah'] ;
+
+const messege: string = 'Welcome to Typescript Class: '
+
+for(let i=0; i<Names.length; i++){
+
+    console.log(messege + Names[i]);
+
+}
