8cf46f75952ba5e18bd8ea669d3ba28f6b453086
hafiz-huzaifah <164788023+hafiz-huzaifah@users.noreply.github.com>
2024-03-31 16:36:01 +0500

    Add files via upload

---
+++ 38_Cities.ts
@@ -0,0 +1,12 @@
+
+
+function describe_city(city: string, country:string = 'Default Country.'){
+
+    console.log(` ${city} is in ${country}`);
+}
+
+describe_city("Karachi","Pakistan");
+
+describe_city("Ghaza","Palestien");
+
+describe_city("Paris");
