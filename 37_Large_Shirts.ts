f274256a1dba7ad47992c1a0f65a67a01048f389
hafiz-huzaifah <164788023+hafiz-huzaifah@users.noreply.github.com>
2024-03-31 16:35:25 +0500

    Add files via upload

---
+++ 37_Large_Shirts.ts
@@ -0,0 +1,13 @@
+
+
+function make_shirt(size:string = 'Large', text:string = 'I love Typescript'){
+
+    console.log(`Creating a ${size} shirt with the messege: ${text}`);
+}
+
+make_shirt();
+
+make_shirt('Medium');
+
+make_shirt('Small','I love Python');
+
