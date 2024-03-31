6c6a032a8c3d027f295264032e282e36afced013
hafiz-huzaifah <164788023+hafiz-huzaifah@users.noreply.github.com>
2024-03-31 15:59:55 +0500

    Add files via upload

---
+++ 10 Program_1.ts
@@ -0,0 +1,17 @@
+
+
+// Program Written by: huzaifah
+// Current Date: 14-2-2024
+
+// This program will write Hello World
+
+console.log('Hello World');
+
+
+
+// Program Written by: Muhammad Huzaifah Khan 
+// Current Date: 27-2-2024
+
+// This program will add two digits.
+
+console.log(2+2);
