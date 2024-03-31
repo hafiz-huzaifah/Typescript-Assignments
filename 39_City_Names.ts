70a09a8ccb5f5819feed36e4319196e5efd835cd
hafiz-huzaifah <164788023+hafiz-huzaifah@users.noreply.github.com>
2024-03-31 16:36:27 +0500

    Add files via upload

---
+++ 39_City_Names.ts
@@ -0,0 +1,12 @@
+
+
+function city_country(city: string, country: string){
+
+    return `${city},${country}`;
+}
+
+console.log(city_country("Karachi","Pakistan"));
+
+console.log(city_country("Ghaza","Palestien"));
+
+console.log(city_country("Paris","France"));
