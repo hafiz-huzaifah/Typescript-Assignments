24e56a61aebe41e5539a188f633127d526676e28
hafiz-huzaifah <164788023+hafiz-huzaifah@users.noreply.github.com>
2024-03-31 16:31:39 +0500

    Add files via upload

---
+++ 31_No_Users.ts
@@ -0,0 +1,7 @@
+let userNames: string[] = ["admin","Hanzalah","Kashif","Haya","Huzaifah"];
+
+if(userNames.length === 0){
+    console.log("We need to find some users!");
+}else{
+    console.log("All user Names have been removed." + userNames.length);
+}
