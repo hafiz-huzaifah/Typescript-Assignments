e7d23a93c09ee687ca3d6b3f56aa75f4c9a04601
hafiz-huzaifah <164788023+hafiz-huzaifah@users.noreply.github.com>
2024-03-31 16:34:51 +0500

    Add files via upload

---
+++ 36_T-Shirts.ts
@@ -0,0 +1,11 @@
+
+function make_shirt(size:string, text:string){
+
+    console.log(`Creating a ${size} shirt with the messege: ${text}`);
+}
+
+make_shirt("large","Hello world");
+
+make_shirt("medium","Hello world");
+
+make_shirt("small","Hello world");
