ddaf05d456cd0cdb36549654b040962659040781
hafiz-huzaifah <164788023+hafiz-huzaifah@users.noreply.github.com>
2024-03-31 16:25:40 +0500

    Add files via upload

---
+++ 23 Conditional_Test.ts
@@ -0,0 +1,49 @@
+
+// write a series of conditional test.
+
+// creating car variable
+let car :string = 'Subaru' ;
+
+// test number 1
+console.log("Is car === 'Subaru'? I predict True.");
+console.log(car === 'subaru');
+
+// test number 2
+console.log("Is car == 'Subaru'? I predict True.");
+console.log(car == 'subaru');
+
+// test number 3
+console.log("Is car != 'Honda'? I predict True.");
+console.log(car != 'Honda');
+
+// test number 4
+console.log("Is car !== 'ford'? I predict True.");
+console.log(car !== 'ford');
+
+// test number 5
+console.log("Is car.upper case == 'SUBARO'? I predict True.");
+console.log(car.toUpperCase() == 'SUBARO');
+
+// test number 6
+console.log("Is car == 'SUBARO'? I predict False.");
+console.log(car == 'SUBARO');
+
+// test number  7
+console.log("Is car === 'SUBARO'? I predict False.");
+console.log(car === 'SUBARO');
+
+// test number  8
+console.log("Is car === 'train'? I predict False.");
+console.log(car === 'train');
+
+// test number  9
+console.log("Is car === 'bus'? I predict False.");
+console.log(car === 'bus');
+
+// test number  10
+console.log("Is car === 'cycle'? I predict False.");
+console.log(car === 'cycle');
+
+
+
+
